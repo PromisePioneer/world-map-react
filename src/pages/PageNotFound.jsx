9b609c7053d3ea8f4ae91a32bd3270c9779f4b17
demigod66 <fifirman000@gmail.com>
2023-12-30 06:44:51 +0700

    creating a reusable component for navbar,implementing link to linking between routes

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav.jsx";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      Not Found :({" "}
+    </div>
+  );
+}
+
+export default PageNotFound;
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav.jsx";
+
+function Pricing() {
+  return (
+    <div>
+      <PageNav />
+      PRICING
+    </div>
+  );
+}
+
+export default Pricing;
